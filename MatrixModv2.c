d5c44d6dd68066e5899fbbe06ece5eeb15fed0a8
AntiSoundWorld <69455703+AntiSoundWorld@users.noreply.github.com>
2020-08-13 09:39:06 +0300

    edit

--- MatrixModv2.c
+++ MatrixModv2.c
@@ -25,23 +25,23 @@ void CreateMatrix(node_t* head, int rows, int columns)
     node_t* pointerColumns = pointerRows;
 
     printf("insert numbers \n");
-    for (int i = 0; i != rows; i++)
-    {
-        pointerRows->value;
-        scanf("%d", &pointerRows->value);
-        for (int j = 0; j != columns - 1; j++)
-        {
-            pointerColumns->nextColumns = (node_t*)malloc(sizeof(node_t));
-            pointerColumns->nextColumns->value;
-            scanf("%d", &pointerColumns->nextColumns->value);
-            pointerColumns->nextColumns->nextColumns = NULL;
-            pointerColumns = pointerColumns->nextColumns;
-        }
+	for (int i = 0; i != rows; i++)
+	{
+		pointerRows->value;
+		scanf("%d", &pointerRows->value);
+		for (int j = 0; j != columns - 1; j++)
+		{
+			pointerColumns->nextColumns = (node_t*)malloc(sizeof(node_t));
+			pointerColumns->nextColumns->value;
+			scanf("%d", &pointerColumns->nextColumns->value);
+			pointerColumns->nextColumns->nextColumns = NULL;
+			pointerColumns = pointerColumns->nextColumns;
+		}
 		pointerRows->nextRows = (node_t*)malloc(sizeof(node_t));
 		pointerRows->nextRows->nextRows = NULL;
 		pointerRows = pointerRows->nextRows;
 		pointerColumns = pointerRows;
-    }
+	}
 }
 
 void ShowMatrix(node_t * head,int columns)
